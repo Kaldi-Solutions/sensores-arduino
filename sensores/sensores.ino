5c00b5339375e0c359c955267909b2ed11654728
MarceloSPTECH <marcelo.cruzjr@sptech.school>
2022-10-18 14:00:37 -0300

    Alteração no código dos sensores

--- sensores/sensores.ino
+++ sensores/sensores.ino
@@ -20,7 +20,17 @@ void setup(){
 void loop(){
   /**DHT11**/
   float umidade = dht_1.readHumidity();
+  float umidadeProjeto = 
+  float umidade2 = 
+  float umidade3 = 
+  float umidade4 = 
+  float umidade5 = 
   float temperatura = dht_1.readTemperature();
+  float temperaturaProjeto = 0.6 * temperatura - 4;
+  float temperatura2 = temperaturaProjeto * 1.05;
+  float temperatura3 = temperaturaProjeto * 0.95;
+  float temperatura4 = temperaturaProjeto * 0.90;
+  float temperatura5 = temperaturaProjeto * 1.10;
   if (isnan(temperatura) or isnan(umidade)){
     Serial.println("Erro ao ler o DHT");
   }else{
@@ -32,22 +42,23 @@ void loop(){
     Serial.println("°C;");
   }
   
-  /* LM35 */
+  /* LM35 
   leitura_lm35 = analogRead(lm35_pin);
   temperatura = leitura_lm35 * (5.0/1023) * 100;
-  Serial.print("Temperatura: " + temperatura);
-  Serial.println(";");
+  Serial.print("Temperatura: ");
+  Serial.print(temperatura);
+    Serial.println("°C;"); */
   
-  /* Bloco do LDR5 */
-//  leitura_ldr = analogRead(ldr_pin);
-//  Serial.print("Luminosidade: " + leitura_ldr);
-//  Serial.println(";");
+  /* Bloco do LDR5 
+  leitura_ldr = analogRead(ldr_pin);
+  Serial.print("Luminosidade: " + leitura_ldr);
+  Serial.println(";"); */
   
-  /* Bloco do TCRT5000 */
-//  if(digitalRead(switch_pin) == LOW){
-//    Serial.println(1);
-//  } else {
-//    Serial.println(0);
-//  }
+  /* Bloco do TCRT5000
+  if(digitalRead(switch_pin) == LOW){
+  Serial.println(1);
+  } else {
+  Serial.println(0);
+  } */
     delay(4000);
 }
